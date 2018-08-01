e87ebe17225b28c03b19040a581fcd44db4771e3
alexander rymdeko-harvey <a.rymdekoharvey@gmail.com>
2018-08-01 12:47:01 -0400

    build

--- .site/build.sh
+++ .site/build.sh
@@ -1,11 +1,16 @@
-
 # pull new findings 
 git pull
+
 # bring down old resources
 docker-compose down
+
 # clean old DB?
-#  rm -rf data/mongo/*
+rm -rf data/mongo/*
+
 # bring up new resources 
-docker-compose up -d --build
+# docker-compose build --force-rm --no-cache --pull  
+docker-compose up -d
+
 # init new DB
-docker exec -t site_cfdb-api_1 python /root/cfdb-api/init_mongo.py
+docker exec -t site_cfdb-api_1 git clone https://github.com/obscuritylabs/OS-CFDB.git /root/OS-CFDB/
+docker exec -t site_cfdb-api_1 python /root/cfdb-api/init_mongo.py /root/OS-CFDB/
--- .site/cfdb-app/app.py
+++ .site/cfdb-app/app.py
@@ -38,35 +38,56 @@ cache = Cache(app)
 def make_cache_key(*args, **kwargs): return request.url
 
 @app.route('/')
+@cache.cached(timeout=60)
 def index():
+    """Default index page.
+    
+    Renders the index HTML file, uses Jinga Templates
+    for some value.
+    
+    Decorators:
+        app.route
+        cache.cached
+    
+    Returns:
+        [type] -- [description]
+    """
     return render_template('index.html', active='index')
 
 @app.route('/search/', methods=['GET'])
+@cache.cached(timeout=60)
 def search():
     return render_template('search.html', active='search')
 
 @app.route('/search/results', methods=['GET','POST'])
+@limiter.limit("30/minute")
+# @cache.cached(timeout=60, key_prefix=make_cache_key)
 def search_console():
-    q_str = '^' + request.form.get('string')
+    q_str = '^' + str(request.form.get('string'))
     cur = db.findings.find({"finding.findingDetails.findingMatrix.title": { "$regex": q_str, "$options" : 'i'}}, {"_id": False, "finding": True}).limit(10)
     docs = [document for document in cur]
-    print(docs)
     return render_template('search_results.html', active='search', docs=docs)
 
-@app.route('/search/query', methods=['GET'])
+@app.route('/search/query', methods=['GET', 'POST'])
+@limiter.limit("30/minute")
+# @cache.cached(timeout=60, key_prefix=make_cache_key)
 def search_query():
-    q_str = '^' + request.args.get('string')
+    q_str = '^' + str(request.args.get('string'))
     cur = db.findings.find({"finding.findingDetails.findingMatrix.title": { "$regex": q_str, "$options" : 'i'}}, {"_id": False, "finding.findingDetails.findingMatrix": True}).limit(10)
     docs = [document for document in cur]
     return make_response(jsonify(docs), 200)
 
 @app.route('/findings', methods=['GET'])
+@limiter.limit("30/minute")
+@cache.cached(timeout=60)
 def findings():
     cur = db.findings.find({}, {"_id": False})
     docs = [document for document in cur]
     return render_template('findings.html', active='findings', docs=docs)
 
 @app.route('/findings/<string:f_id>', methods=['GET'])
+@limiter.limit("30/minute")
+@cache.cached(timeout=60, key_prefix=make_cache_key)
 def finding_id(f_id):
     cur = db.findings.find_one({"finding.findingDetails.findingMatrix.id": f_id}, {"_id": False})
     return render_template('finding.html', active='findings', doc=cur)
