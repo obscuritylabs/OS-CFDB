27f130b83bc9e1822efe600b66549a0df88e989f
⭕Alexander Rymdeko-Harvey <8761706+killswitch-GUI@users.noreply.github.com>
2018-08-06 12:04:21 -0400

    Update .convert.py

--- .convert.py
+++ .convert.py
@@ -6,9 +6,7 @@ topdir = '.'
  
 # The extension to search for
 exten = '.json'
-
 data = []
-
 for dirpath, dirnames, files in os.walk(topdir):
     for name in files:
         if name.lower().endswith(exten):
